80c001ea07ae41f4abd15d2a2f30f83a57c9adc9
Eric Wong <wsdjeg@outlook.com>
2025-04-23 13:56:16 +0800

    update

--- lua/music-player/init.lua
+++ lua/music-player/init.lua
@@ -1,12 +1,22 @@
 local M = {}
 
-local musics_directory = 'D:\\wsdjeg\\my-blog\\docs\\musics'
+local musics_directory = '~/Music'
 local job = require('job')
 
 local jobid
 
 function M.get_musics()
-    return vim.fn.globpath(musics_directory, '*.mp3', 0, 1)
+    if type(musics_directory) == 'string' then
+        return vim.fn.globpath(musics_directory, '*.mp3', 0, 1)
+    else
+        local rst = {}
+        for _, v in ipairs(musics_directory) do
+            for _, m in ipairs(vim.fn.globpath(v, '*.mp3', 0, 1)) do
+                table.insert(rst, m)
+            end
+        end
+        return rst
+    end
 end
 
 function M.play(m)
@@ -16,6 +26,11 @@ end
 
 function M.stop()
     job.stop(jobid)
+end
+
+function M.setup(opt)
+    -- this can be a string or a list of string
+    musics_directory = opt.directory
 end
 
 return M
